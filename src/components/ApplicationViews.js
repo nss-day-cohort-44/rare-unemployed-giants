c36da3c878bcebbcbb3b87b8390055b9c15eb627
ted <tedmarov@gmail.com>
2021-01-28 13:43:28 -0600

    Category stuff is there

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,6 +7,11 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
         }}>
+        <CategoryProvider>
+            <Route exact path="/categories" render={(props) => {
+                        return <CategoryList history={props.history} />
+                    }} />
+        </CategoryProvider>
         </main>
     </>
 }
