ec0ddc22a0c14004f0e5402b6a3860aa352cf91c
Travis Stevenson <thetechtravis@outlook.com>
2021-01-29 11:37:20 -0600

    Added route for CategoryForm in ApplicationViews. Created empty CategoryForm.css file. Created CategoryForm component. Added button to CategoryList component.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { CategoryProvider } from "./categories/CategoryProvider.js"
 import { CategoryList } from "./categories/CategoryList.js"
+import { CategoryForm } from "./categories/CategoryForm.js"
 import { TagProvider } from "./tags/TagProvider.js"
 import { TagList } from "./tags/TagList.js"
 
@@ -13,13 +14,17 @@ export const ApplicationViews = () => {
         }}>
             <CategoryProvider>
                 <Route exact path="/categories" render={
-                        props => <CategoryList {...props}/>
-                        } />
+                    props => <CategoryList {...props} />
+                } />
+
+                <Route exact path="/categories/form" render={
+                    props => <CategoryForm {...props} />
+                } />
             </CategoryProvider>
 
             <TagProvider>
                 <Route exact path="/tags" render={
-                        props => <TagList {...props}/>
+                    props => <TagList {...props} />
                 } />
             </TagProvider>
         </main>
---
+++ src/components/categories/CategoryForm.js
@@ -0,0 +1,59 @@
+/*
+AUTHOR: Travis Stevenson
+PURPOSE: This module is responsible for presenting the client with a form so
+that they can add a new category to the database via DOM interaction.
+*/
+
+import React, { useContext, useEffect, useRef } from "react"
+import { Link } from "react-router-dom"
+import { CategoryContext } from "./CategoryProvider";
+import "./CategoryForm.css"
+
+export const CategoryForm = (props) => {
+    const { categories, getCategories } = useContext(CategoryContext)
+
+    const categoryLabel = useRef()
+
+    // This function fires when the Save button is clicked
+    const handleSubmit = () => {
+        const newCategory = {
+            "label": categoryLabel.current.value
+        }
+
+        return fetch("http://localhost:8088/categories/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(newCategory)
+        })
+            .then(res => res.json())
+    }
+
+    /*
+        1. Render a form for the client to create a new category.
+        2. When the save button is clicked, convert the client's
+            text input to valid JSON and POST it to the database.
+        3. Redirect the client to CategoryList component.
+    */
+    return (
+        <article className="categories">
+            <div style={{ margin: "0rem 3rem" }}>
+                <h1>Create a new category</h1>
+            </div>
+
+            <form className="form--createCategory" onSubmit={handleSubmit}>
+
+                <input ref={categoryLabel} type="text" name="newCategory" className="form-control"
+                    placeholder="Category name" required autoFocus />
+
+                <button onClick={() => {
+                    handleSubmit()
+                    props.history.push('/categories')
+                }}> Save </button>
+
+            </form>
+        </article>
+    )
+}
